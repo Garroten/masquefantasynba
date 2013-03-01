0da06d27a63e22bc8349b8c5e6f01e86e63c2595
Rafael Garrote Hernández <rafaelgarrote@gmail.com>
2013-03-01 20:45:38 +0100

    added routes.rb chages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
-Nbaweb::Application.routes.draw do  
+Nbaweb::Application.routes.draw do      
 
   resources :players
-
 
   #get "home/index"
   #default route
@@ -14,6 +13,39 @@ Nbaweb::Application.routes.draw do
   resources :assists
 
   #blocks
-  match "/blocks/daybefore"   =>"blocks#daybefore"
+  match "/blocks/daybefore"         =>"blocks#daybefore"
+  match "/blocks/fivedaysbefore"    =>"blocks#fivedaysbefore"
+  match "/blocks/tendaysbefore"     =>"blocks#tendaysbefore"
   resources :blocks
+  
+  #fouls
+  match "/fouls/daybefore"          =>"fouls#daybefore"
+  match "/fouls/fivedaysbefore"     =>"fouls#fivedaysbefore"
+  match "/fouls/tendaysbefore"      =>"fouls#tendaysbefore"
+  resources :fouls
+  
+  #rebounds
+  match "/rebounds/daybefore"       =>"rebounds#daybefore"
+  match "/rebounds/fivedaysbefore"  =>"rebounds#fivedaysbefore"
+  match "/rebounds/tendaysbefore"   =>"rebounds#tendaysbefore"
+  resources :rebounds
+  
+  #steals
+  match "/steals/daybefore"         =>"steals#daybefore"
+  match "/steals/fivedaysbefore"    =>"steals#fivedaysbefore"
+  match "/steals/tendaysbefore"     =>"steals#tendaysbefore"
+  resources :steals
+  
+  #turnovers
+  match "/turnovers/daybefore"      =>"turnovers#daybefore"
+  match "/turnovers/fivedaysbefore" =>"turnovers#fivedaysbefore"
+  match "/turnovers/tendaysbefore"  =>"turnovers#tendaysbefore"
+  resources :turnovers
+  
+  #turnovers
+  match "/scores/daybefore"         =>"scores#daybefore"
+  match "/scores/fivedaysbefore"    =>"scores#fivedaysbefore"
+  match "/scores/tendaysbefore"     =>"scores#tendaysbefore"
+  resources :scores
+  
 end
